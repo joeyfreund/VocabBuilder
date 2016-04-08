5d73ad219fa968f5d433f671ddbedca2e3d57d2e
--add <jordan_li80@hotmail.com>
2016-04-07 23:52:37 -0400

    Reposition main activity UI locations

--- app/src/main/java/charlesli/com/personalvocabbuilder/io/MainActivity.java
+++ app/src/main/java/charlesli/com/personalvocabbuilder/io/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends ActionBarActivity {
     private ImageButton mMyVocabButton;
     private ImageButton mReviewButton;
     private ImageButton mTestButton;
+    private ImageButton mSettingsButton;
+    private ImageButton mDictionaryButton;
     private ImageButton mCategoriesButton;
 
 
@@ -28,7 +30,14 @@ public class MainActivity extends ActionBarActivity {
         mMyVocabButton = (ImageButton) findViewById(R.id.myVocabButton);
         mReviewButton = (ImageButton) findViewById(R.id.reviewButton);
         mTestButton = (ImageButton) findViewById(R.id.testButton);
+        mSettingsButton = (ImageButton) findViewById(R.id.settings_button);
+        mDictionaryButton = (ImageButton) findViewById(R.id.dictionary_button);
         mCategoriesButton = (ImageButton) findViewById(R.id.categories_button);
+
+
+        mDictionaryButton.setVisibility(View.INVISIBLE);
+        mSettingsButton.setVisibility(View.INVISIBLE);
+        mTestButton.setVisibility(View.INVISIBLE);
 
         mMyVocabButton.setOnClickListener(new View.OnClickListener() {
             @Override
